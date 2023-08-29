e79ef9b3decde83d36d7f4e81711a268e0299b4d
flztjl <ssrsrfy.jl@gmail.com>
2023-08-28 19:03:09 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/one_product/:id", controller: "products", action: "one_product"
+  get "/all_products", controller: "products", action: "all_products"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create([{ id: 1, name: "apple", price: 2, image_url: "https://as1.ftcdn.net/v2/jpg/01/76/97/96/
+                        1000_F_176979696_hqfioFYq7pX13dmiu9ENrpsHZy1yM3Dt.jpg", description: "apple is a fruit" },
+                { id: 2, name: "orange", price: 3, image_url: "https://images.pexels.com/photos/161559/background-bitter-breakfast-bright-161559.jpeg?cs=srgb&dl=pexels-pixabay-161559.jpg&fm=jpg", description: "orange is a fruit" }])
