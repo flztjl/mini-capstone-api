988304aa5afab0bf403b8e1b84364d0c8d03af1a
flztjl <ssrsrfy.jl@gmail.com>
2023-10-10 10:42:57 +0800

    small fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone-frontend-9yz9.onrender.com/", "localhost:5173"
+    origins "mini-capstone-frontend-9yz9.onrender.com", "localhost:5173"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
